85e61093c070183ec130c4cca80b095453f6d84f
sohailB <bhattisohail20@gmail.com>
2023-05-10 11:52:23 +0500

    code updated

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import algoliasearch from "algoliasearch/lite";
+import React, { Component } from "react";
+import {
+  ClearRefinements,
+  Configure,
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+} from "react-instantsearch-dom";
+import "./App.css";
+import { HitWithInsights } from "./HitWithInsights";
+
+const searchClient = algoliasearch(
+  "B1G2GM9NG0",
+  "aadef574be1f9252bb48d4ea09b5cfe5"
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ais-InstantSearch">
+        <h1>Shopping</h1>
+        <InstantSearch indexName="demo_ecommerce" searchClient={searchClient}>
+          <div className="left-panel">
+            <ClearRefinements />
+            <h2>Brands</h2>
+            <RefinementList attribute="brand" />
+            <Configure hitsPerPage={8} clickAnalytics />
+          </div>
+          <div className="right-panel">
+            <SearchBox />
+            <Hits hitComponent={HitWithInsights} />
+            <Pagination />
+          </div>
+        </InstantSearch>
+      </div>
+    );
+  }
+}
+
+export default App;

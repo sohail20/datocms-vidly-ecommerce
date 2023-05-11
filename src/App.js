cd94ef53016d2216443fccc16ece3671cae81444
sohailB <bhattisohail20@gmail.com>
2023-05-11 17:44:34 +0500

    code

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="ais-InstantSearch">
-        <h1>Shopping</h1>
+        {/* <h1>Shopping</h1> */}
         <InstantSearch indexName="dev_test" searchClient={searchClient}>
           <div className="left-panel">
             <ClearRefinements />
